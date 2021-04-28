c1409b7b07330a76b86f4fdcf0c5524c519148fb
黄克凡 <w545459363@126.com>
2021-04-28 15:46:11 +0800

    commit

--- src/main/java/com/tilitili/service/MainService.java
+++ src/main/java/com/tilitili/service/MainService.java
@@ -1,17 +1,20 @@
 package com.tilitili.service;
 
+import com.tilitili.common.entity.Recommend;
 import com.tilitili.common.entity.VideoData;
 import com.tilitili.common.entity.VideoInfo;
+import com.tilitili.common.entity.query.RecommendQuery;
 import com.tilitili.common.entity.query.VideoDataQuery;
 import com.tilitili.common.entity.query.VideoInfoQuery;
 import com.tilitili.common.manager.VideoDataManager;
+import com.tilitili.common.mapper.RecommendMapper;
 import com.tilitili.common.mapper.VideoInfoMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,11 +22,13 @@ public class MainService {
 
     private final VideoDataManager videoDataManager;
     private final VideoInfoMapper videoInfoMapper;
+    private final RecommendMapper recommendMapper;
 
     @Autowired
-    public MainService(VideoDataManager videoDataManager, VideoInfoMapper videoInfoMapper) {
+    public MainService(VideoDataManager videoDataManager, VideoInfoMapper videoInfoMapper, RecommendMapper recommendMapper) {
         this.videoDataManager = videoDataManager;
         this.videoInfoMapper = videoInfoMapper;
+        this.recommendMapper = recommendMapper;
     }
 
     public List<VideoData> getRankList() {
@@ -33,12 +38,28 @@ public class MainService {
     }
 
     public List<VideoData> getRecommendList() {
-        List<VideoInfo> randomVideoInfoList = videoInfoMapper.random(new VideoInfoQuery().setPageSize(8));
-        return randomVideoInfoList.parallelStream().map(videoInfo -> {
+        ArrayList<VideoData> videoDataList = new ArrayList<>();
+        // 上榜
+        Integer newIssue = videoDataManager.getNewIssue();
+        VideoDataQuery query = new VideoDataQuery().setIssue(newIssue).setPageSize(2);
+        List<VideoData> rankList = videoDataManager.randomRanked(query);
+        videoDataList.addAll(rankList);
+        // 随机
+        List<VideoInfo> randomVideoInfoList = videoInfoMapper.random(new VideoInfoQuery().setPageSize(4));
+        List<VideoData> randomList = randomVideoInfoList.parallelStream().map(videoInfo -> {
             VideoData videoData = videoDataManager.getVideoNewDataByAvOrDefault(videoInfo.getAv());
             BeanUtils.copyProperties(videoInfo, videoData);
             return videoData;
         }).collect(Collectors.toList());
+        videoDataList.addAll(randomList);
+        //推荐
+        List<Recommend> recommendList = recommendMapper.random(new RecommendQuery().setPageSize(2));
+        List<VideoData> recommendVideoList = recommendList.parallelStream().map(Recommend::getAv).map(videoDataManager::getVideoNewDataByAv).collect(Collectors.toList());
+        videoDataList.addAll(recommendVideoList);
+
+        videoDataList.sort((a, b) -> Math.toIntExact(a.getAv() - b.getAv()));
+
+        return videoDataList;
     }
 
 }
--- src/main/java/com/tilitili/service/PlayService.java
+++ src/main/java/com/tilitili/service/PlayService.java
@@ -1,14 +1,13 @@
 package com.tilitili.service;
 
-import com.tilitili.common.entity.Owner;
-import com.tilitili.common.entity.VideoData;
-import com.tilitili.common.entity.VideoInfo;
-import com.tilitili.common.entity.VideoTagRelation;
+import com.tilitili.common.entity.*;
+import com.tilitili.common.entity.query.RecommendQuery;
 import com.tilitili.common.entity.query.VideoDataQuery;
 import com.tilitili.common.entity.query.VideoInfoQuery;
 import com.tilitili.common.entity.query.VideoTagRelationQuery;
 import com.tilitili.common.manager.VideoDataManager;
 import com.tilitili.common.mapper.OwnerMapper;
+import com.tilitili.common.mapper.RecommendMapper;
 import com.tilitili.common.mapper.VideoInfoMapper;
 import com.tilitili.common.mapper.VideoTagRelationMapper;
 import com.tilitili.common.utils.Asserts;
@@ -16,6 +15,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,13 +25,15 @@ public class PlayService {
     private final OwnerMapper ownerMapper;
     private final VideoDataManager videoDataManager;
     private final VideoInfoMapper videoInfoMapper;
+    private final RecommendMapper recommendMapper;
 
     @Autowired
-    public PlayService(VideoTagRelationMapper videoTagRelationMapper, OwnerMapper ownerMapper, VideoDataManager videoDataManager, VideoInfoMapper videoInfoMapper) {
+    public PlayService(VideoTagRelationMapper videoTagRelationMapper, OwnerMapper ownerMapper, VideoDataManager videoDataManager, VideoInfoMapper videoInfoMapper, RecommendMapper recommendMapper) {
         this.videoTagRelationMapper = videoTagRelationMapper;
         this.ownerMapper = ownerMapper;
         this.videoDataManager = videoDataManager;
         this.videoInfoMapper = videoInfoMapper;
+        this.recommendMapper = recommendMapper;
     }
 
     public List<String> listTagList(Long av) {
@@ -46,11 +48,27 @@ public class PlayService {
     }
 
     public List<VideoData> listRecommend() {
-        List<VideoInfo> randomVideoInfoList = videoInfoMapper.random(new VideoInfoQuery().setPageSize(10));
-        return randomVideoInfoList.parallelStream().map(videoInfo -> {
+        ArrayList<VideoData> videoDataList = new ArrayList<>();
+        // 上榜
+        Integer newIssue = videoDataManager.getNewIssue();
+        VideoDataQuery query = new VideoDataQuery().setIssue(newIssue).setPageSize(3);
+        List<VideoData> rankList = videoDataManager.randomRanked(query);
+        videoDataList.addAll(rankList);
+        // 随机
+        List<VideoInfo> randomVideoInfoList = videoInfoMapper.random(new VideoInfoQuery().setPageSize(5));
+        List<VideoData> randomList = randomVideoInfoList.parallelStream().map(videoInfo -> {
             VideoData videoData = videoDataManager.getVideoNewDataByAvOrDefault(videoInfo.getAv());
             BeanUtils.copyProperties(videoInfo, videoData);
             return videoData;
         }).collect(Collectors.toList());
+        videoDataList.addAll(randomList);
+        //推荐
+        List<Recommend> recommendList = recommendMapper.random(new RecommendQuery().setPageSize(2));
+        List<VideoData> recommendVideoList = recommendList.parallelStream().map(Recommend::getAv).map(videoDataManager::getVideoNewDataByAv).collect(Collectors.toList());
+        videoDataList.addAll(recommendVideoList);
+
+        videoDataList.sort((a, b) -> Math.toIntExact(a.getAv() - b.getAv()));
+
+        return videoDataList;
     }
 }
